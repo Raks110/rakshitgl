5fc7a97a76e59d26dabd051c6f754ad39474af7d
Rakshit G L <rakshitgl@gmail.com>
2024-10-03 16:37:25 -0500

    Added: Affiliations and jsconfig

--- src/dashboard/bio/Bio.js
+++ src/dashboard/bio/Bio.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { ThemeContext } from '../../common/theme/ThemeContext';
 import colors from '../../common/colors';
 import './Bio.css'
+import Affiliation from './affiliations/Affiliation';
 
 function Bio() {
     const { darkTheme } = useContext(ThemeContext);
@@ -33,7 +34,9 @@ function Bio() {
                     marginTop: 16,
                     padding: 8
                 }}>
-                    Hi! I am Rakshit. I am a graduate student at the University of Chicago, and I am super stoked about deploying my skills into projects that require a deep understanding of software engineering, cloud computing, and machine learning!
+                    <span style={{
+                        color: darkTheme ? colors.dark.text_color : colors.light.text_color
+                    }}><b>Hi! I am Rakshit.</b></span><br/>I am a graduate student at the University of Chicago, and I am super stoked about deploying my skills into projects that require a deep understanding of software engineering, cloud computing, and machine learning!
                 </div>
                 <div className='logos' style={{
                     backgroundColor: primaryBackground,
@@ -41,9 +44,15 @@ function Bio() {
                     padding: 8,
                     borderRadius: 12
                 }}>
-                    <img className='affiliate_logo' src={require('../../static/uchicago.png')} width={22} height={26}/>
-                    <img className='affiliate_logo' src={require('../../static/deutsche_bank' + (darkTheme ? '_dark' : '') + '.png')} width={20} height={22}/>
-                    <img className='affiliate_logo' style={{marginLeft: 4}} src={require('../../static/manipal' + (darkTheme ? '_dark' : '') + '.png')} width={darkTheme ? 22 : 28} height={darkTheme ? 26 : 30}/>
+                    <Affiliation imageConfig={{height: 26, width: 22, tooltip: '[2024-2025*] M.S. in Computer Science degree from the University of Chicago'}}
+                                 logo={{darkTheme: darkTheme, darkLogo: require('../../static/uchicago.png'), lightLogo: require('../../static/uchicago.png')}}
+                    />
+                    <Affiliation style={{marginLeft: darkTheme ? 10 : 13}} imageConfig={{height: 22, width: 20, tooltip: '[2021-2024] Associate Software Engineer at Deutsche Bank.'}}
+                                 logo={{darkTheme: darkTheme, darkLogo: require('../../static/deutsche_bank_dark.png'), lightLogo: require('../../static/deutsche_bank.png')}}
+                    />
+                    <Affiliation imageConfig={{height: darkTheme ? 26 : 30, width: darkTheme ? 22 : 28, tooltip: '[2017-2021] B. Tech degree from Manipal Academy of Higher Education'}}
+                                 logo={{darkTheme: darkTheme, darkLogo: require('../../static/manipal_dark.png'), lightLogo: require('../../static/manipal.png')}}
+                    />
                 </div>
             </div>
         </>
